5bed74ea00f626263c11a28abddc5f4b062f1ba7
jokot <jokotriyanto123@outlook.com>
2018-10-02 21:32:11 +0700

    [jokot] multiplied function

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,14 +1,21 @@
+import kotlin.test.asserter
+
 fun added(a:Int, b:Int):Int {
     return a+b
 }
 fun reduced(a:Int, b:Int):Int{
     return a-b
 }
+fun multiplied(a:Int,b:Int):Int{
+    return a*b
+}
 fun main(args: Array<String>) {
     val a=10
     val b=10
     println("a = $a")
     println(b)
-    print("a+b = ${added(a,b)}")
-    print("a*b = ${reduced(a,b)}")
+    println("a+b = ${added(a,b)}")
+    println("a-b = ${reduced(a,b)}")
+    println("a*b = ${multiplied(a,b)}")
+
 }
