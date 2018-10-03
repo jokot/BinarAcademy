712f7bebac2f93820d6fdb202d5c9cec16a414cc
jokot <jokotriyanto123@outlook.com>
2018-10-03 20:48:52 +0700

    [jokot] Array Any function

--- src/ArrayAny.kt
+++ src/ArrayAny.kt
@@ -1,6 +1,16 @@
 
 fun main(args: Array<String>) {
     val a= arrayOf(1,2,"yeah",'t',true,1f)
-    
+    ArrayAny(a)
 }
-
+fun ArrayAny(array:Array<Any>){
+    for (i in array){
+        when(i){
+            is Int -> println("$i is Integer")
+            is Char -> println("$i is Character")
+            is Boolean -> println("$i is Boolean")
+            is String -> println("$i is String")
+            is Float -> println("$i is Float")
+        }
+    }
+}
